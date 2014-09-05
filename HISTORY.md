f7e3739a518311262d8803bb158f452294220862
parhelium <me@parhelium.pl>
2014-09-05 12:08:39 +0200

    initial commit

---
+++ HISTORY.md
@@ -0,0 +1,3 @@
+## 1.0.0 (2014-09-05)
+- support for js-schema@0.7.0
+- package created
---
+++ package.js
@@ -0,0 +1,10 @@
+Package.describe({
+  summary: "js-schema library package",
+  version: "1.0.0",
+  git: ""
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.0.1');
+  api.addFiles('js-schema.min.js');
+});
